ac664cef8a23d99c63d7b2fe52dfca412e5f0982
user 154 <Hudayousef97>
2023-07-21 23:17:00 +0300

    finish project

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
 
-
+startBtn.onclick = () =>{
+    popupInfo.classList.add('active');
+    
+}
 
 
 
